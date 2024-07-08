edc63bb2466c51b78bf6d581100d4f372ed920b8
Huu Ho <huu.ho@sjsu.edu>
2024-07-08 13:17:52 -0700

    Add package.json and other configuration files

---
+++ pages/_app.tsx
@@ -0,0 +1,42 @@
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import React, {useEffect} from "react";
+import {magic} from "@/lib/magic-client";
+import {useRouter} from "next/router";
+import Loading from "@/components/loading/loading";
+
+export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+  const [isLoading, setIsLoading] = React.useState(true);
+
+  useEffect(() => {
+    const handleComplete = () => {
+      setIsLoading(false);
+    };
+    router.events.on("routeChangeComplete", handleComplete);
+    router.events.on("routeChangeError", handleComplete);
+
+    return () => {
+      router.events.off("routeChangeComplete", handleComplete);
+      router.events.off("routeChangeError", handleComplete);
+    };
+  }, [router]);
+
+
+  useEffect(() => {
+    const handleLoggedIn = async () => {
+      const isLoggedIn = await magic.user.isLoggedIn();
+      if (isLoggedIn) {
+        // route to /
+        router.push("/");
+      } else {
+        // route to /login
+        router.push("/login");
+      }
+    };
+    handleLoggedIn();
+  }, []);
+
+  return isLoading? <Loading/>
+      : <Component {...pageProps} />;
+}
